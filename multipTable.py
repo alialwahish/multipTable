2d9d71cd00f06ab531956489cdc2525d83fb6db0
Ali Bayati <bayati.ali@icloud.com>
2018-04-30 16:04:57 -0500

    initial commit

---
+++ multipTable.py
@@ -0,0 +1,23 @@
+from sys import stdout
+
+li=["x"]
+y =1
+for i in range(1,13):
+    li.append(i)
+
+for b in li:
+    print b,"\t",
+
+print("")    
+for v in range(1,13):
+    
+    print v,"\t",
+    for j in range(1,13):
+        
+        print j*v,"\t",
+    print("")   
+      
+   
+   
+    
+   
